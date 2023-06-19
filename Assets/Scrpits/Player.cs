a73796bfff48e530188388d68566cada93837a88
zyyyxxx <729494344@qq.com>
2023-06-19 16:52:17 +0800

    Initial

---
+++ Assets/Scrpits/Player.cs
@@ -0,0 +1,157 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public static Player Instance { get; private set; }
+ 
+   
+    
+    public event EventHandler<OnSelectedCounterChangedEventArgs> OnSelectedCounterChanged;
+    public class OnSelectedCounterChangedEventArgs : EventArgs
+    {
+        public ClearCount selectedCounter;
+    }
+    
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private GameInput gameInput;
+    [SerializeField] private LayerMask countersLayerMask;
+    
+    private bool isWalking;
+    private Vector3 lastInteractDir;
+    private ClearCount selectedCounter;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("There is more than one Player instance");
+        }
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        gameInput.OnInteractAction += GameInput_OnInteractAction;
+    }
+
+    private void GameInput_OnInteractAction(object sender, EventArgs e)
+    {
+        if (selectedCounter != null)
+        {
+            selectedCounter.Interact();
+        }
+    }
+
+
+    // Update is called once per frame
+    private void Update()
+    {
+        HandleMovement();
+        HandleInteractions();
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
+
+    private void HandleInteractions()
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        
+        // 防止不移动后射线检测失效
+        if (moveDir != Vector3.zero)
+        {
+            lastInteractDir = moveDir;
+        }
+
+        float interactDistance = 2f;
+        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance , countersLayerMask))
+        {
+            if (raycastHit.transform.TryGetComponent(out ClearCount clearCount))
+            {
+                // Has ClearCounter
+                if (clearCount != selectedCounter)
+                {
+                    SetSelectedCounter(clearCount);
+                }
+            }else {
+                SetSelectedCounter(null);
+            }
+        }else {
+            SetSelectedCounter(null);
+        }
+    }
+
+    private void HandleMovement()
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        float moveDistance = Time.deltaTime * moveSpeed;
+        float playerRadius = .7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight , playerRadius, moveDir ,moveDistance);
+
+        if (!canMove)
+        {   
+            // 我们希望贴着墙壁进行对角线移动时，可以左右移动
+            
+            // Cannot move towards moveDir
+            
+            // Attempt only X movement
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight , playerRadius, moveDirX ,moveDistance);
+
+            if (canMove)
+            {
+                moveDir = moveDirX;
+            }
+            else
+            {
+                // 无法在X轴移动，尝试Z轴
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight , playerRadius, moveDirZ ,moveDistance);
+
+                if (canMove)
+                {
+                    moveDir = moveDirZ;
+                }
+                else
+                {
+                    // 完全不能移动
+                }
+            }
+            
+        }
+        
+        if (canMove)
+        {
+            transform.position += moveDir * moveDistance;    
+        }
+        
+        isWalking = moveDir != Vector3.zero;
+        
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward , moveDir , Time.deltaTime * rotateSpeed);
+    }
+
+    private void SetSelectedCounter(ClearCount selectedCounter)
+    {
+        this.selectedCounter = selectedCounter;
+        
+        OnSelectedCounterChanged?.Invoke(this  , new OnSelectedCounterChangedEventArgs
+        {
+            selectedCounter =  selectedCounter
+        });
+    }
+
+    
+}
---
+++ Assets/Scrpits/SelectCounterVisual.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectCounterVisual : MonoBehaviour
+{
+    [SerializeField] private ClearCount clearCounter;
+    [SerializeField] private GameObject visualGameObject;
+    private void Start()
+    {
+        Player.Instance.OnSelectedCounterChanged += Instance_OnSelectedCounterChanged;
+    }
+
+    private void Instance_OnSelectedCounterChanged(object sender, Player.OnSelectedCounterChangedEventArgs e)
+    {
+        if (e.selectedCounter == clearCounter)
+        {
+            Show();
+        }
+        else
+        {
+            Hide();
+        }
+    }
+
+    private void Show()
+    {
+        visualGameObject.SetActive(true);   
+    }
+
+    private void Hide()
+    {
+        visualGameObject.SetActive(false);
+    }
+}
